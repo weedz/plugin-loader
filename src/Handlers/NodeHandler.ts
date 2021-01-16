4ca29c66334de8388a9922652d84828428354cfc
Linus Björklund <weedzcokie@gmail.com>
2021-01-16 21:43:54 +0100

    remove "index.ts"-suffix from NodeHandler

--- src/Handlers/NodeHandler.ts
+++ src/Handlers/NodeHandler.ts
@@ -1,6 +1,6 @@
 import { HandlerArgument } from "..";
 
 export async function NodeHandler<T, API>({ manifest, path, api, dependencies }: HandlerArgument<T, API>) {
-    const plugin = await import(`${path}/${manifest.pluginPath || manifest.name}/index.ts`);
+    const plugin = await import(`${path}/${manifest.pluginPath || manifest.name}`);
     return new plugin.default(api, dependencies) as T;
 }
