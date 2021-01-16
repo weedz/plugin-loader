0ec2f871a00d5ded289d8ab94f23423c4b7519e1
Linus Björklund <weedzcokie@gmail.com>
2021-01-16 20:41:56 +0100

    Add list of dependencies as a second type argument on PluginBase

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -2,11 +2,11 @@ type Dependencies<API> = {
     [key: string]: PluginBase<API>;
 }
 
-export abstract class PluginBase<API> {
+export abstract class PluginBase<API, D = Dependencies<API>> {
     protected api: API;
-    protected dependencies: Dependencies<API>;
-    constructor(api: API, dependencies?: Dependencies<API>) {
+    protected dependencies: D;
+    constructor(api: API, dependencies: D) {
         this.api = api;
-        this.dependencies = dependencies || {};
+        this.dependencies = dependencies;
     }
 }
